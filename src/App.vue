15e56dda4d13d829af4f0c83e4456be06b362421
Eugene <zanevskijzenia@gmail.com>
2025-10-23 22:35:53 +0300

    Init routing

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { RouterView } from 'vue-router'
+import AppLayout from './layouts/AppLayout.vue';
+</script>
+
+<template>
+  <main class="container">
+    <AppLayout>
+      <RouterView />
+    </AppLayout>
+    <!-- 
+    <AppHeader />
+    <RouterView />
+    <AppFooter />
+    -->
+    <Button>
+      Click
+      <img src="@/assets/logo.svg" alt="" />
+    </Button>
+  </main>
+</template>
+
+<style scoped>
+.container {
+  max-width: 80rem;
+  width: 100%;
+  margin: 0 auto;
+}
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,53 @@
+<script setup>
+    import { RouterLink } from 'vue-router';
+</script>
+
+<template>
+    <header class="header">
+        <RouterLink class="header__logo" to="/">
+            <img alt="Vue logo" class="header__logo-img" src="@/assets/logo.svg" width="125" height="125" />
+        </RouterLink>
+
+        <nav class="header__navigation">
+            <RouterLink class="header__navigation-link" to="/">Home</RouterLink>
+            <RouterLink class="header__navigation-link" to="/about">About</RouterLink>
+        </nav>
+    </header>
+</template>
+
+<style scoped>
+    .header{
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        padding: 1rem 0;
+    }
+
+    .header__logo{
+        width: 3rem;
+    }
+
+    .header__logo-img{
+        max-width: 3rem;
+        width: 100%;
+        height: 100%;
+    }
+
+    .header__navigation{
+        display: flex;
+        gap: 2rem;
+    }
+
+    .header__navigation-link{
+        text-decoration: none;
+        color: #000;
+    }
+
+    .header__navigation-link.router-link-active{
+        color: #0000ff;
+    }
+
+    .header__navigation-link:hover{
+        color: rgb(81, 81, 169);
+    }
+</style>
